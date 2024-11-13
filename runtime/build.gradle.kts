571bdd5ec94b3f708641c52ef5fb56047036bdbe
Baiqin Wang <bwang@fubo.tv>
2024-11-13 15:54:15 -0500

    Make runtime KMM.

--- runtime/build.gradle.kts
+++ runtime/build.gradle.kts
@@ -1,6 +1,9 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
-    id("java-library")
-    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.mavenPublish)
 }
 
 java {
@@ -8,8 +11,67 @@ java {
     targetCompatibility = JavaVersion.VERSION_11
 }
 
+group = "com.decomposer.runtime"
+version = "0.0.1"
+
 kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+    jvm("desktop")
+
+    androidTarget {
+        publishLibraryVariants("release")
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(libs.kotlin.test)
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+            }
+        }
+        val androidUnitTest by getting {
+            dependencies {
+            }
+        }
+        val desktopMain by getting {
+            dependencies {
+            }
+        }
+        val desktopTest by getting {
+            dependencies {
+            }
+        }
+    }
+}
+
+android {
+    namespace = "com.decomposer.runtime"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 }

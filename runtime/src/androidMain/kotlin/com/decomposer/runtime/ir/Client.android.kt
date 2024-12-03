c8fd4ac2c3a0c129994f68585fc8fa7e8e850f3b
Baiqin Wang <bwang@fubo.tv>
2024-12-03 18:14:40 -0500

    Added some polymorphic serializer.

--- runtime/src/androidMain/kotlin/com/decomposer/runtime/ir/Client.android.kt
+++ runtime/src/androidMain/kotlin/com/decomposer/runtime/ir/Client.android.kt
@@ -1,21 +1,11 @@
 package com.decomposer.runtime.ir
 
-import com.decomposer.runtime.CommandHandler
 import com.decomposer.runtime.Logger
 import com.decomposer.runtime.compose.CompositionExtractor
-import com.decomposer.runtime.connection.AbstractKtorClient
 import com.decomposer.runtime.connection.AbstractOkHttpClient
 import com.decomposer.runtime.connection.ConnectionContract
 import com.decomposer.runtime.connection.model.DeviceDescriptor
 import com.decomposer.runtime.connection.model.DeviceType
-
-internal class AndroidKtorClient(
-    serverPort: Int = ConnectionContract.DEFAULT_SERVER_PORT,
-    commandHandlers: Set<CommandHandler> = emptySet()
-) : AbstractKtorClient(serverPort, commandHandlers), Logger by AndroidLogger {
-    override fun buildDeviceDescriptor(): DeviceDescriptor =
-        DeviceDescriptor(deviceType = DeviceType.ANDROID)
-}
 
 internal class AndroidOkHttpClient(
     serverPort: Int = ConnectionContract.DEFAULT_SERVER_PORT,
--- runtime/src/commonMain/kotlin/com/decomposer/runtime/connection/ConnectionContract.kt
+++ runtime/src/commonMain/kotlin/com/decomposer/runtime/connection/ConnectionContract.kt
@@ -2,7 +2,6 @@ package com.decomposer.runtime.connection
 
 object ConnectionContract {
     const val HEADER_DEVICE_TYPE = "X-Device-Type"
-    const val HEADER_SERIAL_NUMBER = "X-Serial-Number"
     const val DEFAULT_SERVER_PORT = 9801
     const val DEFAULT_CONNECTION_PATH = "/connect"
 }

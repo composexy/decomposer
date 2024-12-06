e22a259e113180c7e9859503ee31804f1c21ba77
Biking Wang <baiqin.fubotv@gmail.com>
2024-12-06 16:11:07 -0500

    Add popup and dialog in sample for testing.

--- sample/src/main/kotlin/com/decomposer/sample/MainActivity.kt
+++ sample/src/main/kotlin/com/decomposer/sample/MainActivity.kt
@@ -2,40 +2,22 @@ package com.decomposer.sample
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.Popup
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        /*
-        setContent {
-            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                Column {
-                    Greeting(
-                        message = "Hello Decomposer!",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                    Footer(
-                        message = "Bye!",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }*/
         ComposeView(this).also {
             setContentView(it)
         }.setContent {
@@ -47,6 +29,14 @@ class MainActivity : ComponentActivity() {
                 item {
                     Text("Lazy")
                 }
+            }
+            Popup {
+                Text("Popup")
+            }
+            Dialog(
+                onDismissRequest = {}
+            ) {
+                Text("Dialog")
             }
         }
     }
